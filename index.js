e9795caa33dc8b0d3b6c1e9f66cd7d59ebeed026
Ruma Das <rumaadl16@gmail.com>
2020-04-25 03:07:34 +0930

    Adding homework week_9 file in repository

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-const questions = [
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
-
-function writeToFile(fileName, data) {
-
-
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your Project Title?"
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?"
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
+  ]);
 }
 
-function init() {
-    inquirer.prompt().then(data => {
-        console.log(data);
-    })
 
+
+async function init() {
+  console.log("hi")
+  try {
+    const answers = await promptUser();
+
+
+    await writeFileAsync("readme.md", text);
+
+    console.log("Successfully wrote to index.html");
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 init();
